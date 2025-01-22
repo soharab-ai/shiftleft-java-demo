aec3ac46850c0e40f69ebf1c341905b824e83b84
soharab-ai <soharab.infracloud@qwiet.ai>
2025-01-22 15:58:14 +0530

    Fixing src/main/java/io/shiftleft/controller/SearchController.java for finding 13

--- src/main/java/io/shiftleft/controller/SearchController.java
+++ src/main/java/io/shiftleft/controller/SearchController.java
@@ -17,16 +17,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SearchController {
 
-  @RequestMapping(value = "/search/user", method = RequestMethod.GET)
-  public String doGetSearch(@RequestParam String foo, HttpServletResponse response, HttpServletRequest request) {
+@RequestMapping(value = "/search/user", method = RequestMethod.GET)
+@Validated
+public String doGetSearch(@RequestParam @SafeHtml @Value("${foo}") String foo, HttpServletResponse response, HttpServletRequest request) {
     java.lang.Object message = new Object();
     try {
-      ExpressionParser parser = new SpelExpressionParser();
-      Expression exp = parser.parseExpression(foo);
-      message = (Object) exp.getValue();
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression(foo);
+        message = (Object) exp.getValue();
     } catch (Exception ex) {
-      System.out.println(ex.getMessage());
+        System.out.println(ex.getMessage());
     }
+    return message.toString();
+}
+
     return message.toString();
   }
 }
